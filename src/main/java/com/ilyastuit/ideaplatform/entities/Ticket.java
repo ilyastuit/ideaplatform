d70fb0002ea10d47dc5b846cabaf646420754d20
ilyastuit <ilyastuit@gmail.com>
2021-01-29 14:09:25 +0500

    Added tickets

---
+++ src/main/java/com/ilyastuit/ideaplatform/entities/Ticket.java
@@ -0,0 +1,28 @@
+package com.ilyastuit.ideaplatform.entities;
+
+import java.util.Date;
+
+public class Ticket {
+
+    private String origin;
+
+    private String originName;
+
+    private String destination;
+
+    private String destinationName;
+
+    private Date departureDate;
+
+    private String departureTime;
+
+    private Date arrivalDate;
+
+    private String arrivalTime;
+
+    private String carrier;
+
+    private int stops;
+
+    private double price;
+}

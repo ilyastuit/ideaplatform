bbfebefada8ef6e18b21e2c10902bcbba637f503
ilyastuit <ilyastuit@gmail.com>
2021-01-29 14:14:49 +0500

    Ticket POJO class - updated

--- src/main/java/com/ilyastuit/ideaplatform/entities/Ticket.java
+++ src/main/java/com/ilyastuit/ideaplatform/entities/Ticket.java
@@ -1,6 +1,8 @@
 package com.ilyastuit.ideaplatform.entities;
 
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.Objects;
 
 public class Ticket {
 
@@ -14,15 +16,147 @@ public class Ticket {
 
     private Date departureDate;
 
-    private String departureTime;
+    private LocalTime departureTime;
 
     private Date arrivalDate;
 
-    private String arrivalTime;
+    private LocalTime arrivalTime;
 
     private String carrier;
 
     private int stops;
 
     private double price;
+
+    public Ticket(String origin, String originName, String destination, String destinationName, Date departureDate, LocalTime departureTime, Date arrivalDate, LocalTime arrivalTime, String carrier, int stops, double price) {
+        this.origin = origin;
+        this.originName = originName;
+        this.destination = destination;
+        this.destinationName = destinationName;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.arrivalDate = arrivalDate;
+        this.arrivalTime = arrivalTime;
+        this.carrier = carrier;
+        this.stops = stops;
+        this.price = price;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getOriginName() {
+        return originName;
+    }
+
+    public void setOriginName(String originName) {
+        this.originName = originName;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public int getStops() {
+        return stops;
+    }
+
+    public void setStops(int stops) {
+        this.stops = stops;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return stops == ticket.stops && Double.compare(ticket.price, price) == 0 && Objects.equals(origin, ticket.origin) && Objects.equals(originName, ticket.originName) && Objects.equals(destination, ticket.destination) && Objects.equals(destinationName, ticket.destinationName) && Objects.equals(departureDate, ticket.departureDate) && Objects.equals(departureTime, ticket.departureTime) && Objects.equals(arrivalDate, ticket.arrivalDate) && Objects.equals(arrivalTime, ticket.arrivalTime) && Objects.equals(carrier, ticket.carrier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(origin, originName, destination, destinationName, departureDate, departureTime, arrivalDate, arrivalTime, carrier, stops, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "origin='" + origin + '\'' +
+                ", originName='" + originName + '\'' +
+                ", destination='" + destination + '\'' +
+                ", destinationName='" + destinationName + '\'' +
+                ", departureDate=" + departureDate +
+                ", departureTime=" + departureTime +
+                ", arrivalDate=" + arrivalDate +
+                ", arrivalTime=" + arrivalTime +
+                ", carrier='" + carrier + '\'' +
+                ", stops=" + stops +
+                ", price=" + price +
+                '}';
+    }
 }
